d290fa3580517b2fa30c2826d2be60a5d173e7d1
mikaylarandall <mikayla.mae@icloud.com>
2024-03-20 10:37:19 -0600

    deleting files

--- obj/Release/net8.0/Darla.AssemblyInfo.cs
+++ obj/Release/net8.0/Darla.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Darla")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0106a2ba0d59e1e875839fe19cf18cd250230e47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Darla")]
