5707de66650e950f1f142f84ca9cbe5a8ef3a3b8
caitlynstoked <119529327+caitlynstoked@users.noreply.github.com>
2024-03-25 09:15:30 -0600

    fixed again :/

--- obj/Debug/net8.0/Darla.AssemblyInfo.cs
+++ obj/Debug/net8.0/Darla.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Darla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6648313b1d0c3bd8afee266bd9970ac63aa41f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbdac7fefa4dba206b1fe51cae093b4df18557d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Darla")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Darla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
