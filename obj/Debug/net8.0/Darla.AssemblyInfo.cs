7ed33d0993d9e11da399a8747857e7098e5dc62c
Tbsheff <92263166+Tbsheff@users.noreply.github.com>
2024-03-25 09:28:59 -0600

    commit

--- obj/Debug/net8.0/Darla.AssemblyInfo.cs
+++ obj/Debug/net8.0/Darla.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Darla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9625f6374438df62fb3b307dffd588329ebff8f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8702a1d0ed5ffb822ab0f7678d8f4ca4931c1a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Darla")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Darla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
