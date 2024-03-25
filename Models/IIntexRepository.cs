63d3214477612e56b37680e306b54a8d0ecf95ee
garrettjash <garrettashcroft@gmail.com>
2024-03-25 09:08:00 -0600

    Finished I and EF

--- Models/IIntexRepository.cs
+++ Models/IIntexRepository.cs
@@ -2,5 +2,14 @@
 {
     public interface IIntexRepository
     {
+        IEnumerable<Assignment> Assignments { get; }
+        IEnumerable<Grade> Grades { get; }
+        IEnumerable<JudgeRoom> JudgeRooms { get; }
+        IEnumerable<Permission> Permissions { get; }
+        IEnumerable<PresentationSurvey> PresentationSurveys { get; }
+        IEnumerable<RoomSchedule> RoomSchedules { get; }
+        IEnumerable<StudentTeam> StudentTeams { get; }
+        IEnumerable<UserPassword> UserPasswords { get; }
+        IEnumerable<User> Users { get; }
     }
 }
