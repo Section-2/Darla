04f9d0d13dabfb58b985e632231267b135e44521
elijahaken <elijahaken73@gmail.com>
2024-03-25 08:52:21 -0600

    yeet

--- obj/Debug/net8.0/Darla.AssemblyInfo.cs
+++ obj/Debug/net8.0/Darla.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Darla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31fea4caf12df85ff45e8e1d8f754d2380aefd72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Darla")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Darla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
