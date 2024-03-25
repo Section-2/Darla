f1b43a197719124e8cef57170d5f4d8bb48580bd
ryanhawkins <ryanhawkins@RYANHAWKINS5C1C>
2024-03-25 09:36:09 -0600

    practice 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Darla.Controllers
             return View();
         }
 
+        public IActionResult AddEditTA()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
